52f9b9850fc20b923a1b1612c2804db8223378a3
Andrew Evans <andy@heyzap.com>
2012-04-06 16:25:18 -0700

    Don't monkeypatch Array#sum - unnecessary & destructive

--- lib/classifier/extensions/vector.rb
+++ lib/classifier/extensions/vector.rb
@@ -7,15 +7,15 @@ require 'matrix'
 require 'mathn'
 
 class Array
-  def sum(identity = 0, &block)
-    return identity unless size > 0
-  
-    if block_given?
-      map(&block).sum
-    else
-      inject { |sum, element| sum + element }.to_f
-    end
-  end
+  # def sum(identity = 0, &block)
+  #   return identity unless size > 0
+  # 
+  #   if block_given?
+  #     map(&block).sum
+  #   else
+  #     inject { |sum, element| sum + element }.to_f
+  #   end
+  # end
 end
 
 class Vector
